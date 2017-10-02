f6f5f4e2081945e2d95102d785fcd6adce0ded77
arivanandan <uarivanandan@gmail.com>
2017-10-02 17:59:38 +0530

    Fixes put test

--- src/test/fixtures/put/expected.js
+++ src/test/fixtures/put/expected.js
@@ -2,5 +2,5 @@ import _isotropyDb from "isotropy-lib-keyvaluedb";
 
 
 async function putTodo() {
-  await _isotropyDb.put("redis://127.0.0.1:6379", { Get_Eggs: "Get liek 2" });
+  await _isotropyDb.put("redis://127.0.0.1:6379", { key: "Get_Eggs", value: "Get liek 2" });
 }
--- src/test/fixtures/put/fixture.js
+++ src/test/fixtures/put/fixture.js
@@ -1,5 +1,5 @@
 import myDb from "../my-db";
 
 async function putTodo() {
-  myDb.todos = myDb.todos.concat({ Get_Eggs: "Get liek 2" });
+  myDb.todos = myDb.todos.concat({ key: "Get_Eggs", value: "Get liek 2" });
 }
